b07460dd3f95d76fd196e6e912b91a337c25e829
BeslanU <92beslan@gmail.com>
2020-11-01 10:23:43 -0600

    Here we go again

--- src/main/java/Superman.java
+++ src/main/java/Superman.java
@@ -2,5 +2,6 @@ public class Superman {
     public static void main(String[] args) {
         System.out.println("superman");
         System.out.println("not the super");
+        System.out.println("supergirl");
     }
 }
