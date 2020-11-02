dd7a2a1752bab06c6e81fe076c7f2e7fbbdf4bf5
Beslan <68976914+BeslanU@users.noreply.github.com>
2020-11-01 20:31:12 -0600

    Update and rename Superman.java to Superman.js

--- src/main/java/Superman.java
+++
@@ -1,7 +0,0 @@
-public class Superman {
-    public static void main(String[] args) {
-        System.out.println("superman");
-        System.out.println("not the super");
-        System.out.println("supergirl");
-    }
-}
---
+++ src/main/java/Superman.js
@@ -0,0 +1,8 @@
+var x = 9.656;
+x.toExponential(2);     // returns 9.66e+0
+x.toExponential(4);     // returns 9.6560e+0
+x.toExponential(6);     // returns 9.656000e+0
+var x = 123;
+x.toString();            // returns 123 from variable x
+(123).toString();        // returns 123 from literal 123
+(100 + 23).toString();   // returns 123 from expression 100 + 23
